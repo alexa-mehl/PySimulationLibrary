4e6a1004cda2dae5f5d0b1047b19dfb0444cc292
Amir Czwink <amirc@win.tu-berlin.de>
2016-07-27 19:40:49 +0200

    -added license -configured setup

--- PySimLib/SimulationResult.py
+++ PySimLib/SimulationResult.py
@@ -1,4 +1,28 @@
 """
+  Copyright (C) 2014-2016  Alexandra Mehlhase <a.mehlhase@tu-berlin.de>, All Rights Reserved
+  
+  Implemented by Alexandra Mehlhase, Amir Czwink
+  
+  This file is part of the AMSUN project
+  (https://gitlab.tubit.tu-berlin.de/groups/amsun)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+   
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+
+"""
 This class holds the values that a simulation generates.
 The values in the object are stored in a dictionary that maps each variable to a vector of values of this variable.
 The following should illustrate that:
--- setup.py
+++ setup.py
@@ -1,23 +1,38 @@
-from distutils.core import setup
 """
-from distutils.command.install import INSTALL_SCHEMES
+  Copyright (C) 2014-2016  Alexandra Mehlhase <a.mehlhase@tu-berlin.de>, All Rights Reserved
+  
+  Implemented by Alexandra Mehlhase, Amir Czwink
+  
+  This file is part of the AMSUN project
+  (https://gitlab.tubit.tu-berlin.de/groups/amsun)
 
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib'] + "/PySimLib"
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+   
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+from setuptools import setup, find_packages;
+
 setup(
-    # Application name:
     name="PySimLib",
-
-    # Version number (initial):
-    version="1.0",
-
-    # Application author details:
+    version="1.0.0a1",
+    description="The Python Simulation Library allows to simulate Modelica and Simulink models in a platform-independent way.",
+    url = "https://gitlab.tubit.tu-berlin.de/a.mehlhase/PySimulationLibrary",
     author="Alexandra Mehlhase",
     author_email="a.mehlhase@tu-berlin.de",
+    packages = ["PySimLib", "PySimLib.Exceptions", "PySimLib.Mat", "PySimLib.Models", "PySimLib.Solvers", "PySimLib.Tools"],
+    license = "GPL",
     
-    description="Python Simulation Library",
-
-    # Packages
-    packages=["PySimLib", "PySimLib.Exceptions", "PySimLib.Mat", "PySimLib.Models", "PySimLib.Solvers", "PySimLib.Tools"],
+    install_requires = [
+        "matplotlib"
+    ],
 )
